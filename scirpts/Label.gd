85947c97cd743c4e0b77a9a15b7ead3b01300262
Martin Bauer <m.bauer@student.tugraz.at>
2020-02-14 22:38:34 +0100

    fonts.

---
+++ scirpts/Label.gd
@@ -0,0 +1,14 @@
+extends Label
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_color_override("custom_colors/font_color", Color("8d3d61"))
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
